5f89cc16f8c1f09e0a8d70088a41ba9f6059153f
Deepak-developer-dot <130280011+Deepak-developer-dot@users.noreply.github.com>
2026-01-29 21:25:04 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"payout-backend/config"
+	"payout-backend/routes"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	r := gin.Default()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(" Error loading .env file")
+	}
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	config.ConnectDB()
+	routes.RegisterRoutes(r)
+	r.Run(":8080")
+
+}
